c13493fd1435e2136ee8aa2a42e9d1315ce066a9
Alexandre Chabert <alexandre.chabert@fcati.com.br>
2019-08-28 08:48:54 -0300

    Alterado layout e rota de agradecimento

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,27 +6,27 @@
 
 @section('content')
 <div class="text-white">
-    <hr>
+
     <form action="/lead/novo" method="POST">
         {{ csrf_field() }}
         <div class="row">
-            <div class="col form-group">
+            <div class="col input-group-lg">
                 <label for="nome">Nome: </label>
                 <input type="text" class="form-control" name="nome" aria-describedby="nome" placeholder="">
             </div>
-            <div class="col form-group">
+            <div class="col input-group-lg">
                 <label for="email">E-mail: *</label>
                 <input type="text" class="form-control" name="email" aria-describedby="passHelp" placeholder=""
                     required>
             </div>
         </div>
         <div class="row">
-            <div class="col form-group">
+            <div class="col input-group-lg">
                 <label for="empresa">Empresa:</label>
                 <input type="text" class="form-control" name="empresa" aria-describedby="empresa" placeholder="">
             </div>
 
-            <div class="col form-group">
+            <div class="col input-group-lg">
                 <label for="telefone">Telefone: </label>
                 <input type="text" class="form-control telefone_internacional" name="telefone" id="telefone"
                     aria-describedby="telefone" placeholder="+55 11 32301613" value="+55">
@@ -34,10 +34,10 @@
         </div>
             <div class="form-group">
                 <label for="observacao">Observação: </label>
-                <textarea name="observacao" class="form-control" name="observacao" cols="30" rows="7"></textarea>
+                <textarea name="observacao" class="form-control" name="observacao" cols="30" rows="5"></textarea>
             </div>
             <div class="form-group mb-4">
-                <button type="submit" class="btn btn-success pull-right">Enviar</button>
+                <button type="submit" class="btn btn-lg btn-secondary pull-right">Enviar Dados</button>
             </div>
     </form>
     <br>
--- resources/views/layouts/nav_top.blade.php
+++ resources/views/layouts/nav_top.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand" href="http://www.fcati.com.br" style="padding: 5px">
-        <img alt="FCATI" src="{{ asset('images/logo_branco.png') }}" height="40" width="83"> 
+        <img alt="FCATI" src="{{ asset('images/logo_branco.png') }}" height="40" width="83">
     </a>
     {{-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> --}}
-  
+
     <ul class="navbar-nav  px-3">
         <li class="nav-item text-nowrap">
             <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/"></a>
@@ -15,6 +15,6 @@
 <div class="text-center mt-5">
     <a href="http://www.arandaeventos.com.br/eventos2019/netcom/index.html" target="_blank"><img alt="NETCOM logo"  class="img-fluid" src="{{ asset('images/netcom_logo.png') }}" width="300px"> </a>
     <a href="https://www.portalvoip.com.br/" target="_blank"><img alt="portal Voip logo"  class="img-fluid" src="{{ asset('images/portalvoip.png') }}" width="300px"> </a>
-    <a href="https://email360.com.br/" target="_blank"><img alt="email360 logo" class="img-fluid"  src="{{ asset('images/logo360.png') }}" width="300px"> </a> 
-    <a href="https://email360.com.br/" target="_blank"><img alt="zimbra extra logo"  class="img-fluid" src="{{ asset('images/zimbrazextras-transp.png') }}" width="300px">  </a>
+    <a href="https://email360.com.br/" target="_blank"><img alt="email360 logo"  width="250px" class="img-fluid"  src="{{ asset('images/logo360.png') }}" width="300px"> </a>
+    <a href="https://email360.com.br/" target="_blank"><img alt="zimbra extra logo" width="250px" class="img-fluid" src="{{ asset('images/zimbrazextras-transp.png') }}" width="300px">  </a>
 </div>
